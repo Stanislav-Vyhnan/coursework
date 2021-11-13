2683730a02ee85121056bd8c1cd0261a1c4b4035
Stanislav-Vyhnan <stas.vignan@gmail.com>
2021-11-13 14:54:33 +0200

    Project in progress

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
 import Main from './components/Main';
-import Themes from './components/Themes';
 
 export default function App() {
     return (
         <div className="app">
-            <Themes />
             <Main />
         </div>
     );
--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,10 +1,16 @@
 import React, { useRef, useEffect } from 'react';
 import './index.css';
 import Result from '../Result';
+
 export default function WriteTxt() {
     const textRef = useRef(null);
     const fragmentRef = useRef(null);
-
+    const regRef = useRef(null);
+    const strictRef = useRef(null);
+    const selFragmRef = useRef(null);
+    // regRef.current.checked,
+    // strictRef.current.checked,
+    // selFragmRef.current.checked;
     useEffect(() => textRef.current.focus());
 
     const search = () => {
@@ -26,11 +32,34 @@ export default function WriteTxt() {
                     id="inputTxt"
                     className="writeTxt"
                     ref={textRef}
-                    title="Введіть текст"
                     contentEditable="true"
                 />
             </div>
             <div className="serch">
+                <b>Налаштування пошуку</b>
+                <div className="settings">
+                    <div className="pin">
+                        <label className="switch">
+                            <input type="checkbox" ref={regRef} />
+                            <span className="slider round"></span>
+                        </label>
+                        <span>Ігнорування реєстру</span>
+                    </div>
+                    <div className="pin">
+                        <label className="switch">
+                            <input type="checkbox" ref={strictRef} />
+                            <span className="slider round"></span>
+                        </label>
+                        <span>Строгий пошук</span>
+                    </div>
+                    <div className="pin">
+                        <label className="switch">
+                            <input type="checkbox" ref={selFragmRef} />
+                            <span className="slider round"></span>
+                        </label>
+                        <span>Виділення фрагменту</span>
+                    </div>
+                </div>
                 <input placeholder="Введіть фрагмент" ref={fragmentRef} />
                 <button onClick={search}>Search</button>
             </div>
